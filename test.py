dd79e4f9f7b83d96fe4878133d4ab6e3dd46ce6f
salehmirnezhad <salehmirnzhadinfo@gmail.com>
2024-08-27 16:03:58 +0000

    Setup project with Flask app, Docker containerization, Ansible deployment, and GitLab CI/CD configuration. Also, edited my_playbook.yml and webhook_listener.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-# بررسی دسترسی به URL
-response = requests.get('http://192.168.200.54:8929/api/v4/projects/1', headers={'PRIVATE-TOKEN': 'glpat-E4fgvLn_yLfh577m8zkx'}, verify=False)
-
-if response.status_code == 200:
-    print("Project found:", response.json())
-else:
-    print("Failed to find project:", response.content)
